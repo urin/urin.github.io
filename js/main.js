d1a77b2695a611790034c03f989f972be4ee9a73
urin <urinkun@gmail.com>
2014-02-23 00:37:22 +0530

    Update posts.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function() {
   $.balloon.defaults = $.extend(true, $.balloon.defaults, {
     classname: "balloon-defaults", minLifetime: 0, tipSize: 8
   });
-  $(".sns-icons a").balloon({ position: "bottom" });
+  $("header .sns-icons a").balloon({ position: "bottom" });
   $(".post, footer").find("[title]").balloon();
   $("#jquery-version").text(" version " + $.fn.jquery);
 });
